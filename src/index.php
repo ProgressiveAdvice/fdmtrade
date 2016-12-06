c76ff85c5f766e2e1ab9281a7aaac057c203d1a0
Szücs Aron <szucs.aron@farmmix.hu>
2016-12-06 15:35:23 +0100

    Greatly improved header

--- src/index.php
+++ src/index.php
@@ -9,77 +9,127 @@
 				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
 				integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-			<!--link type="text/css" media="all" src="assets/style.css" /-->
+				<!--link type="text/css" media="all" src="assets/style.css" /-->
 			<link rel="stylesheet" type="text/css" media="all" href="assets/custom.css" />
 		</head>
-		<body class="maintenance-page in-maintenance no-sidebars">
-			<div class="page">
-				<div id="header">
-					<div id="logo-title">
+			<body class="maintenance-page in-maintenance no-sidebars">
+				<header class="header-image">
+					<nav class="navbar navbar-default fdm-nav" role="navigation">
+						<div class="container">
 
-						<a href="/" title="Címlap" rel="home" id="logo">
-							<img src="assets/image/header.jpg" alt="Címlap" />
-						</a>
-						<div id="name-and-slogan">
-							<h1 id="site-name">
-								<a href="/" title="Címlap" rel="home">
-									<span>fdmtrade.hu</span>
-								</a>
-							</h1>
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+									<span class="sr-only">Toggle navigation</span>
+									<i class="icon-menu"> Menu</i>
+								</button>
+							</div>
 
+							<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+								<ul class="nav navbar-nav">
+									<li>
+										<a href="/" title="Főoldal">Főoldal</a>
+									</li>
+									<li>
+										<a href="/termekek" title="Termékek">Termékek</a>
+									</li>
+									<li>
+										<a href="/rolunk" title="Rólunk">Rólunk</a>
+									</li>
+									<li>
+										<a href="/ajanlatkeres" title="Ajánlatkérés">Ajánlatkérés</a>
+									</li>
+									<li>
+										<a href="/kapcsolat" title="Kapcsolat">Kapcsolat</a>
+									</li>
+								</ul>				
+							</div>
 						</div>
-						<!-- /name-and-slogan -->
+					</nav>
+
+				</header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				<div class="container">
+					<div id="header">
+						<div id="logo-title">
+
+							<a href="/" title="Címlap" rel="home" id="logo">
+								<img src="assets/image/header.jpg" alt="Címlap" />
+							</a>
+							<div id="name-and-slogan">
+								<h1 id="site-name">
+									<a href="/" title="Címlap" rel="home">
+										<span>fdmtrade.hu</span>
+									</a>
+								</h1>
+
+							</div>
+							<!-- /name-and-slogan -->
+						</div>
+						<!-- /logo-title -->
+
+
 					</div>
-					<!-- /logo-title -->
+					<!-- /header -->
 
-
-				</div>
-				<!-- /header -->
-				
-				<div id="content">
+					<div id="content">
 	content
+					</div>
+					<!-- Egyforma magasság -->
+					<div style="clear:both;"/>
 				</div>
-				<!-- Egyforma magasság -->
-				<div style="clear:both;"/>
-			</div>
 
 
-			<div id="footer">
-				<font color="white">
-					<br>
+				<div id="footer">
+					<font color="white">
 						<br>
 							<br>
 								<br>
 									<br>
-										<p align="right">Fdm Trade Kft.
-											<br>
-  4220 Hajdúböszörmény, Ipartelep u. 18-20.
+										<br>
+											<p align="right">Fdm Trade Kft.
 												<br>
-	Adószám: 25019995-2-09
+  4220 Hajdúböszörmény, Ipartelep u. 18-20.
 													<br>
-  Cégjegyzékszám: 09-09-026349
+	Adószám: 25019995-2-09
 														<br>
+  Cégjegyzékszám: 09-09-026349
 															<br>
+																<br>
   E-mail: info@farmmix.hu
-																<br>
-	Web: www.farmmix.hu</p>
-																<br>
 																	<br>
-																	</div>
+	Web: www.farmmix.hu</p>
+																	<br>
+																		<br>
+																		</div>
 
 
 
 
-																	<div id="footer-lent">
-																		<center>
-																			<font color="white">	
-																				<p>
+																		<div id="footer-lent">
+																			<center>
+																				<font color="white">	
+																					<p>
 	Copyright © 2016 - Minden jog fenntartva.
-																					<br>
 																						<br>
-																						</p>
-																					</center>	
-																				</div>
+																							<br>
+																							</p>
+																						</center>	
+																					</div>
 
 
 
@@ -93,10 +143,10 @@
 
 
 
-																				<!-- <div id="footer">
+																					<!-- <div id="footer">
   </div>-->
-																			</div>
-																			<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"/>
-																			<script type="text/javascript" src="active.js"/>
-																		</body>
-																	</html>
+																				</div>
+																				<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"/>
+																				<script type="text/javascript" src="active.js"/>
+																			</body>
+																		</html>
