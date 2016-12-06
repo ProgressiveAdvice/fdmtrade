096010543126ba9835af89930c392523922acfba
Szücs Aron <szucs.aron@farmmix.hu>
2016-12-06 14:59:47 +0100

    Added index.php. It is NOT finished

---
+++ src/index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="hu">
+	<head>
+		<meta charset="utf-8">
+			<title>FDM Trade Kft</title>
+			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+			<link rel="shortcut icon" href="images/FDM.ico" type="image/vnd.microsoft.icon" />
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
+				integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+			<!--link type="text/css" media="all" src="assets/style.css" /-->
+			<link rel="stylesheet" type="text/css" media="all" href="assets/custom.css" />
+		</head>
+		<body class="maintenance-page in-maintenance no-sidebars">
+			<div class="page">
+				<div id="header">
+					<div id="logo-title">
+
+						<a href="/" title="Címlap" rel="home" id="logo">
+							<img src="assets/image/header.jpg" alt="Címlap" />
+						</a>
+						<div id="name-and-slogan">
+							<h1 id="site-name">
+								<a href="/" title="Címlap" rel="home">
+									<span>fdmtrade.hu</span>
+								</a>
+							</h1>
+
+						</div>
+						<!-- /name-and-slogan -->
+					</div>
+					<!-- /logo-title -->
+
+
+				</div>
+				<!-- /header -->
+				
+				<div id="content">
+	content
+				</div>
+				<!-- Egyforma magasság -->
+				<div style="clear:both;"/>
+			</div>
+
+
+			<div id="footer">
+				<font color="white">
+					<br>
+						<br>
+							<br>
+								<br>
+									<br>
+										<p align="right">Fdm Trade Kft.
+											<br>
+  4220 Hajdúböszörmény, Ipartelep u. 18-20.
+												<br>
+	Adószám: 25019995-2-09
+													<br>
+  Cégjegyzékszám: 09-09-026349
+														<br>
+															<br>
+  E-mail: info@farmmix.hu
+																<br>
+	Web: www.farmmix.hu</p>
+																<br>
+																	<br>
+																	</div>
+
+
+
+
+																	<div id="footer-lent">
+																		<center>
+																			<font color="white">	
+																				<p>
+	Copyright © 2016 - Minden jog fenntartva.
+																					<br>
+																						<br>
+																						</p>
+																					</center>	
+																				</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+																				<!-- <div id="footer">
+  </div>-->
+																			</div>
+																			<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"/>
+																			<script type="text/javascript" src="active.js"/>
+																		</body>
+																	</html>
