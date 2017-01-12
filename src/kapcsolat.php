6ea2ecde32cdd7b755350789ec2f232955653330
Szücs Aron <szucs.aron@farmmix.hu>
2017-01-12 15:46:54 +0100

    Added ImageMagick to Dockerfile so it will be installed. Enhanced .htaccess file to deal with regular php files. Added few new classes to custom.css. Added new kapcsolat.php page

---
+++ src/kapcsolat.php
@@ -0,0 +1,43 @@
+ï»¿<?php	
+	include "header.php";
+?>
+
+<div class="contact-column">
+	<h1>FDM Trade Kft.</h1>
+	<p>4220 Hajdúböszörmény,<br />
+	Ipartelep utca. 18-20.<br />
+	Tel: (+36) 52/123-123<br />
+	Fax: (+36) 52/123-123</p>
+	<p>E-mail:<br /><a href="mailto:info@fdm.hu">info@fdm.hu</a></p>
+	<p>Adószám: 25019995-2-09<br />
+	Cégjegyzék szám: 09-09-026349</p>
+</div>
+
+<div class="contact-column">
+	<h3>Tolna, Baranya, Somogy megye</h3>
+	<p>Baasandavaa Ankhbayar<br />
+	Tel: (+36) 30/511-7795</p>
+	<p>E-mail:<br /><a href="mailto:b.anka@fdmtrade.hu">b.anka@fdmtrade.hu</a></p>
+	<p><br /></p>
+	<form action="send_message.php?recipient=b.anka@fdmtrade.hu">
+		<button class="button">Ãœzenet kÃ¼ldÃ©se</button>
+	</form>
+</div>
+
+<div class="contact-column">
+	<h3>Vas, VeszprÃ©m, Zala megye</h3>
+	<p>KulcsÃ¡r Szilvia<br />
+	Tel: (+36) 30/190-9982</p>
+	<p>E-mail:<br /><a href="mailto:kulcsar.szilvia@fdmtrade.hu">kulcsar.szilvia@fdmtrade.hu</a></p>
+	<p><br /></p>
+	<form action="send_message.php?recipient=kulcsar.szilvia@fdmtrade.hu">      
+		<button class="button">Ãœzenet kÃ¼ldÃ©se</button>
+	</form>
+</div>
+
+<div style="float: left width: 100%">
+	<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d21491.558029234206!2d21.534616!3d47.675782!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x558f5fb0059a78b8!2sFarmmix+Kft.!5e0!3m2!1shu!2shu!4v1480114009113" width="100%" height="350" frameborder="0" style="border:0"></iframe>
+</div>
+<?php
+	include "footer.php";
+?>
